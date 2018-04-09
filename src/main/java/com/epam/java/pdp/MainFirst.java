6938a655502cc4627a5f45ada506a77989bb80ac
yarmihor <yarmigor@gmail.com>
2018-04-09 23:20:36 +0300

    Remove garbage

--- src/main/java/com/epam/java/pdp/MainFirst.java
+++ src/main/java/com/epam/java/pdp/MainFirst.java
@@ -38,47 +38,4 @@ public class MainFirst {
         }
     }
 
-//    private static void max(int a, int b, int c, int d) {
-//
-//        int max = a;
-//
-//        if (b > max)
-//            max = b;
-//        if (c > max)
-//            max = c;
-//        if (d > max)
-//            max = d;
-//        System.out.println("Max value "+max);
-//        }
-//
-//    private static void evenNumbersWhile (){
-//        int e = 100;
-//        while ( 1 <= e ) {
-//               if (e % 2==0) {
-//                System.out.print(" " + e);
-//            }
-//            e = e - 1;
-//        }
-//    }
-//    private static void evenNumbersFor (){
-//        int i;
-//        int e = 100;
-//        for (i=1;i<=e;) {
-//            if (i % 2==0) {
-//                System.out.println(" " + i);
-//            }
-//            i = ++i;
-//        }
-//    }
-//
-//    private static void evenNumbersDoWhile (){
-//        int e = 100;
-//        do  {
-//            if (e % 2==0) {
-//                System.out.print(" " + e);
-//            }
-//            e = e - 1;
-//        } while (1<=e);
-//    }
-
 }
